cb331555c5d999aa419cee294c529fe924d96f9c
mike2ox <firefinger07@gmail.com>
2019-01-07 13:13:42 +0900

    [upload]1000.cpp

---
+++ ACM/Baekjoon/Basic/1000.cpp
@@ -0,0 +1,20 @@
+/*
+*	author : mike2ox
+*	BOJ : 1000
+*	descript : https://www.acmicpc.net/problem/
+*	type : I/O
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio("false");
+
+	int a, b;
+
+	cin >> a >> b;
+	cout << a + b << '\n';
+
+	return 0;
+}
