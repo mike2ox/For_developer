1baac39f1373820a5a3778eacc4e3f5336946d4e
mike2ox <firefinger07@gmail.com>
2019-01-07 13:17:13 +0900

    [upload]10951 문제

---
+++ ACM/Baekjoon/Basic/10951.cpp
@@ -0,0 +1,22 @@
+/*
+*	author : mike2ox
+*	BOJ : 10951
+*	descript : https://www.acmicpc.net/problem/
+*	type : I/O
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio("false");
+
+	int a, b;
+
+	while (cin >> a >> b) {
+		cout << a + b << '\n';
+	}
+	
+	system("pause");
+	return 0;
+}
