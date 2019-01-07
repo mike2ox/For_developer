e675a9d6f84094c3a4641764f57793a2e98862c6
mike2ox <firefinger07@gmail.com>
2019-01-07 13:17:42 +0900

    [upload]2558 문제

---
+++ ACM/Baekjoon/Basic/2558.cpp
@@ -0,0 +1,21 @@
+/*
+*	author : mike2ox
+*	BOJ : 2558
+*	descript : https://www.acmicpc.net/problem/
+*	type : I/O
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio("false");
+
+	int a, b;
+
+	cin >> a;
+	cin >> b;
+	cout << a + b << '\n';
+
+	return 0;
+}
