41ba47dceb817a1fa86ea2c844ed495f57662d48
mike2ox <firefinger07@gmail.com>
2019-01-07 21:10:39 +0900

    [upload]11719 문제

---
+++ ACM/Baekjoon/Basic/11719.cpp
@@ -0,0 +1,23 @@
+/*
+*	author : mike2ox
+*	BOJ : 11719
+*	descript : https://www.acmicpc.net/problem/
+*	type : I/O
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+string str;
+
+int main() {
+
+	ios_base::sync_with_stdio("false");
+
+	while (getline(cin, str)) {
+		cout << str << '\n';
+	}
+
+	system("pause");
+	return 0;
+}
