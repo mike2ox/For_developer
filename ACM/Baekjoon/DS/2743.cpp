044670c7da00043e8c042881d67ed6a092be02a9
mike2ox <firefinger07@gmail.com>
2019-01-15 17:30:12 +0900

    [upload] 2743

---
+++ ACM/Baekjoon/DS/2743.cpp
@@ -0,0 +1,25 @@
+/*
+*	author : mike2ox
+*	BOJ : 2743
+*	descript : https://www.acmicpc.net/problem/
+*	type : DS
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+
+	string str;
+	cin >> str;
+
+	cout << str.length() << '\n';
+
+	system("pause");
+	return 0;
+}
+/*
+	string 내부 length 함수를 파악하고 있는지 확인
+*/
