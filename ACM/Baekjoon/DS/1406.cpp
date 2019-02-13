f0baf0a3124895c84f547db6d438404717e72dab
mike2ox <firefinger07@gmail.com>
2019-02-13 21:35:46 +0900

    [TODO] 1406 에디터 시간 오버

---
+++ ACM/Baekjoon/DS/1406.cpp
@@ -0,0 +1,22 @@
+/*
+*	author : mike2ox
+*	BOJ : 1406
+*	descript : https://www.acmicpc.net/problem/
+*	type : DS
+*/
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+
+	system("pause");
+	return 0;
+}
+/*
+	
+*/
