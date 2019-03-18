267004ad45b13baae7aad791ad3334cb606fc7c4
mike2ox <firefinger07@gmail.com>
2019-03-18 19:14:04 +0900

    [TODO] 15686 인구이동

---
+++ ACM/Baekjoon/SW기출/16234.cpp
@@ -0,0 +1,24 @@
+/*
+*	author : mike2ox
+*	BOJ : 15686
+*	descript : https://www.acmicpc.net/problem/15686
+*	type : ªÔº∫ ±‚√‚ : bfs
+*/
+#include<iostream>
+#include<math.h>
+#include<queue>
+using namespace std;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+
+
+	return 0;
+}
+/*
+
+*/
