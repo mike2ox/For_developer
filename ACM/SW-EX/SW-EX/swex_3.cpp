87a32df51aae3f630b027746fdcf2f6fd59efd21
mike2ox <firefinger07@gmail.com>
2019-04-01 19:52:21 +0900

    [TODO] 특이한 자석, 삼성 기출과 유사

---
+++ ACM/SW-EX/SW-EX/swex_3.cpp
@@ -0,0 +1,29 @@
+/*
+*	author : mike2ox
+*	swex : 특이한 자석
+*	descript : https://www.swexpertacademy.com/main/learn/course/lectureProblemViewer.do
+*	type : 완전 탐색
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<cstring>
+#define p pair<int,int>
+using namespace std;
+
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int TC;
+	cin >> TC;
+
+
+
+
+	return 0;
+}
