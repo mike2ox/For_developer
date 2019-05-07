14d7c6320ac6de33a5b14b2a786e367595b5cbda
mike2ox <firefinger07@gmail.com>
2019-05-07 23:46:55 +0900

    [check] 2667 단지번호 붙이기, 계속 오류

--- ACM/Baekjoon/Graph_1/2667.cpp
+++ ACM/Baekjoon/Graph_1/2667.cpp
@@ -5,38 +5,35 @@
 *	type : dfs
 */
 #include<iostream>
-#include<queue>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int map[25][25];
-bool ck[25][25];
+int map[26][26];
+bool ck[26][26];
 
 const int dy[] = { 0,0,-1,1 };
 const int dx[] = {1, -1, 0, 0};
 
 int n;
 int cnt;	//각 단지내 집 수
-queue<int> q; // 오름차순 단지 내 수
+vector<int> v; // 오름차순 단지 내 수
 
 //좌표, 단지 수
-void dfs(int _i, int _j, int ncnt) {
-	//종료 조건
-	// 이제 더 갈 곳이 없는 경우
-	if () {
-		q.push(cnt);
-		return;
-	}
+void dfs(int _i, int _j) {
+	cnt++;
+	ck[_i][_j] = true;
+
 	for (int i = 0; i < 4; ++i) {
 		int ny = _i + dy[i];
 		int nx = _j + dx[i];
 
 		//범위 밖 + 이전에 방문한 기록이 있는 경우
-		if (ny<0 || ny>n - 1 || nx<0 || nx>n - 1 || ck[ny][nx])
-			continue;
-
-		ck[ny][nx] = true;
-		dfs(ny, nx, ncnt+1);
-		ck[ny][nx] = false;		
+		if (ny>=0&&nx>=0&& ny<n&&nx<n){
+			if (map[ny][nx]==1&&ck[ny][nx]==false) {
+				dfs(ny, nx);
+			}
+		}
 	}
 
 	return;
@@ -51,19 +48,26 @@ int main() {
 
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j) {
-			cin >> map[i][j];
+			scanf_s("%1d", &map[i][j]);
 		}
 	}
 
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j < n; ++j) {
 			// 완전 탐색, 1이고 방문안한 경우
-			if(map[i][j]&&ck[i][j])
-				dfs(i,j,0);
+			if (map[i][j]==1 && !ck[i][j]) {
+				dfs(i, j);
+				v.push_back(cnt);
+				cnt = 0;
+			}
 		}
 	}
 
+	sort(v.begin(), v.end());
+	cout << v.size() << '\n';
 
+	for (int i = 0; i < (int)v.size(); i++)
+		cout << v[i] << '\n';
 
 	system("pause");
 	return 0;
