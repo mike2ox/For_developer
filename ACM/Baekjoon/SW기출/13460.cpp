7020f328aa2bab7db1b97328d840006c612b0a0a
mike2ox <firefinger07@gmail.com>
2019-05-12 16:23:41 +0900

    [init] re-coding

--- ACM/Baekjoon/SW기출/13460.cpp
+++ ACM/Baekjoon/SW기출/13460.cpp
@@ -6,42 +6,14 @@
 *	hint :
 */
 #include<iostream>
-#include<utility>
 #include<queue>
 using namespace std;
-
-char ck[11][11];
-int N, M;
-int dx[] = { -1, 0, 1, 0 };
-int dy[] = { 0, -1, 0, 1 };
-int solve;	//Å»Ãâ ÃÖ¼Ò È½¼ö
-
 
 int main() {
 
 	ios_base::sync_with_stdio("false");
-
-	queue<pair<int, int>> move;
-
-	//init
-	for (int i = 0; i < 11; i++)
-		for (int j = 0; j < 11; j++)
-			ck[i][j] = '#';
-
-	cin >> N >> M;
+	cin.tie(NULL);
 	
-	for (int i = 1; i <= N; i++)
-		for (int j = 1; j <= M; j++)
-			cin >> ck[i][j];
-
-	while (1) {
-		
-		move.push(make_pair(1,3));
-	}
-
-
-
-	system("pause");
 	return 0;
 }
 /*
