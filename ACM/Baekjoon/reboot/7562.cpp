f301b90283ec75765692102af8c53321e12a8dfd
mike2ox <firefinger07@gmail.com>
2020-01-31 14:48:15 +0900

    Clear 나이트의 이동(+20m)
    - visit check 위치 변경
    - visit map 크기 변경
    - cnt 선언 변경(global -> struct node 안으로)
    - 시작지점 == 목적지점 조건 구현부 수정

--- ACM/Baekjoon/reboot/7562.cpp
+++ ACM/Baekjoon/reboot/7562.cpp
@@ -1,34 +1,40 @@
 // BOJ no. 7562, mike2ox(2020)
 #include<iostream>
-// #include<vector>
+#include<vector>
 #include<queue>
 using namespace std;
 
 const int dy[] = { 1,1,2,2,-1,-1,-2,-2 };
 const int dx[] = { 2,-2,1,-1,2,-2,1,-1 };
 
-bool visit[300][300];
+bool visit[301][301];
 int testCase;
 int len;	//체스판 크기
 int cy, cx, desY, desX;	//현재 위치, 목적지 위치
+vector<int> ret;
+
+struct node {
+	int curY, curX;
+	int cnt;
+};
 
 void bfs() {
 
-	queue<pair<int, int>> night;
-	int cnt = 1;
+	queue<node> night;
+	int depth = 1;
 	int len_q = 1;
-	night.push({ cy, cx });
+	night.push({ cy, cx, 0});
+	visit[cy][cx] = true;
 
 	while (!night.empty()) {
 		if (len_q == 0) {
-			cnt++;
+			depth++;
 			len_q = night.size();
 		}
-		int nowY = night.front().first;
-		int nowX = night.front().second;
-
+		int nowY = night.front().curY;
+		int nowX = night.front().curX;
+		int nowCnt = night.front().cnt;
 		night.pop();
-		visit[nowY][nowX] = true;
 		--len_q;
 
 		for (int i = 0; i < 8; ++i) {
@@ -38,13 +44,14 @@ void bfs() {
 			if (ny<0 || nx <0 || ny>len || nx>len)
 				continue;
 			if (ny == desY && nx == desX) {
-				cout << cnt << '\n';
+				ret.push_back(nowCnt+1);
 				return;
 			}
 			if (visit[ny][nx])
 				continue;
 
-			night.push({ ny, nx });
+			night.push({ ny, nx, nowCnt+1 });
+			visit[ny][nx] = true;
 		}
 	}
 
@@ -67,15 +74,16 @@ int main() {
 			}
 		}
 
-		if (cy == desY && cx == desX) {
-			cout << 0 << '\n';
-			continue;
-		}
+		if (cy == desY && cx == desX)
+			ret.push_back(0);
+		else
+			bfs();
 
-		bfs();
+		
 	}
 
-	
-	system("pause");
+	for (int i =0; i <ret.size(); ++i)
+		cout << ret[i] << '\n';
+
 	return 0;
 }
