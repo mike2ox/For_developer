6b4d4609b9e2058fb58236ba5f9c07b95cd26969
mike2ox <firefinger07@gmail.com>
2020-02-13 18:16:20 +0900

    Check exception conditions(+85m)

--- ACM/Baekjoon/reboot/14226.cpp
+++ ACM/Baekjoon/reboot/14226.cpp
@@ -3,80 +3,63 @@
 #include<queue>
 using namespace std;
 
-int s;
-struct node {
-	int value;
-	int copyValue;
-	int cnt;
-};
 bool visit[1001][1001];
+struct step {
+	int curTime;
+	int clipboard;
+	int printValue;
+};
+int s;
 
-int bfs() {
-	queue<node> q;
-	q.push({ 1 , 0,0});
+void bfs() {
+	queue<step> q;
+	q.push({ 0,0,1 });
+	// visit[0][1] = true;
+	int qSize = 0;
+	int	minimum = 100000;
 
 	while (!q.empty()) {
-		int value = q.front().value;
-		int plus = q.front().copyValue;
-		int cCnt = q.front().cnt;
-		node newData;
-
+		if (qSize == 0) {
+			if (minimum != 100000)
+				break;
+			qSize = q.size();
+		}
+		step current = q.front();
 		q.pop();
+		--qSize;
 
-		// 종료 조건
-		if (s == value)
-			return cCnt;
-		
-		if (cCnt == 0) {
-			newData.value = value;
-			newData.copyValue = value;
-			newData.cnt = cCnt + 1;
-			visit[newData.value][newData.copyValue] = true;
-			q.push(newData);
-		}
-		else if (cCnt == 1) {
-			newData.value = value+plus;
-			newData.copyValue = plus;
-			newData.cnt = cCnt + 1;
-			if (visit[newData.value][newData.copyValue])
-				continue;
-			visit[newData.value][newData.copyValue] = true;
-			q.push(newData);
-		}
-		else {
-			//1. 이전 클립보드 붙이기
-			newData.value = value + plus;
-			newData.copyValue = plus;
-			newData.cnt = cCnt + 1;
-			if (visit[newData.value][newData.copyValue]||newData.value>1000)
-				continue;
-			q.push(newData);
-
-			//2. 클립보드 갱신하고 붙이기
-			plus = value;
-			newData.value = value + plus;
-			newData.copyValue = plus;
-			newData.cnt = cCnt + 2;
-			if (visit[newData.value][newData.copyValue] || newData.value > 1000)
-				continue;
-			q.push(newData);
-		}
-		//3. 1개 삭제 삭제
-		newData.value = value - 1;
-		if (visit[newData.value][newData.copyValue] || newData.value > 1000)
+		if (visit[current.clipboard][current.printValue])
 			continue;
-		newData.cnt = cCnt + 1;
-		q.push(newData);
+		visit[current.clipboard][current.printValue] = true;
+
+
+		if (current.printValue == s && minimum > current.curTime) {
+			minimum = current.curTime;
+			//break;
+		}
+		else if (current.printValue + current.clipboard <= s) {
+			//1. 화면복사(클립보드와 같은경우 배제)
+			if (current.clipboard != current.printValue) {
+				q.push({ current.curTime + 1, current.printValue, current.printValue });
+				//3. 삭제
+				q.push({ current.curTime + 1, current.clipboard, current.printValue - 1 });
+			}
+			//2. 붙여넣기
+			if (current.clipboard != 0) {
+				q.push({ current.curTime + 1, current.clipboard, current.printValue + current.clipboard });
+			}
+		}
 	}
+
+	cout << minimum << '\n';
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cout.tie(0);
-
 	cin >> s;
 
-	cout << bfs();
+	bfs();
 
 	return 0;
 }
