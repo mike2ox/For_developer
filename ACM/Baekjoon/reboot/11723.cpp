c81b10532c79835c7a06bd1f4036f7c0a6d93b98
mike2ox <firefinger07@gmail.com>
2020-02-19 16:06:08 +0900

    Cleared 11723 집합(+a), 비트 연산자 적용해서 해결

--- ACM/Baekjoon/reboot/11723.cpp
+++ ACM/Baekjoon/reboot/11723.cpp
@@ -1,85 +1,46 @@
 // BJO no. 11723, mike2ox(2020)
 #include<iostream>
 #include<string>
-#include<vector>
 using namespace std;
-#define DIFF 48
 
-int m;
-vector<int> v;
-bool visit[21];
-int check(int x) {
-	if (visit[x]) {
-		cout << "1\n";
-		return 1;
-	}
-	else {
-		cout << "0\n";
-		return 0;
-	}
-}
-void add(int x) {
-	if (!visit[x]) {
-		visit[x] = true;
-		v.push_back(x);
-	}
-	else
-		return;
-}
-void remove(int x){
-	if (visit[x]) {
-		for (int i = 0; i < v.size(); ++i){
-			if (v[i] == x)
-				v.erase(v.begin() + i);
-		}
-		visit[x] = false;
-	}
-	else
-		return;
-}
-void toggle(int x) {
-	if (visit[x])
-		remove(x);
-	else
-		add(x);
-}
-void all() {
-	v.clear();
-	for (int i = 1; i <= 20; ++i) {
-		v.push_back(i);
-		visit[i] = true;
-	}
-}
-void empty() {
-	v.clear();
-	for (int i = 1; i <= 20; ++i) {
-		visit[i] = false;
-	}
-}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	string command,str;
 	int value;
-
+	int n=0, m;
 	cin >> m;
 
 	while (m--) {
 		cin >> command;
-		if (command == "all")
-			all();
-		else if (command == "empty")
-			empty();
-		else {
+		
+		switch (command[0])
+		{
+		case 'a':
+			if (command[1] == 'd') { // add
+				cin >> value;
+				n |= (1 << value);
+				//cout << n << '\n';
+			}
+			else if (command[1] == 'l') {	//all
+				n = (1 << 21) - 1;
+			}
+			break;
+		case 'r': // remove
 			cin >> value;
-			if (command == "check")
-				check(value);
-			else if (command == "toggle")
-				toggle(value);
-			else if (command == "add")
-				add(value);
-			else if (command == "remove")
-				remove(value);
+			n &= ~(1 << value);
+			break;
+		case 'c':
+			cin >> value;
+			cout << ((n & (1 << value)) == 0 ? 0 : 1) << '\n';
+			break;
+		case 't':
+			cin >> value;
+			n ^= (1 << value);
+			break;
+		case 'e':
+			n = 0;
+			break;
 		}
 	}
 
