e17cc4b26d6beb3f9fe81ed186d7f800feb52654
mike2ox <firefinger07@gmail.com>
2020-09-02 11:27:22 +0900

    Clear 베스트앨범(66분)

---
+++ ACM/Programmers/level_3/42579.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+bool compare(pair<string, int> pre, pair<string, int> next)
+{
+	return pre.second > next.second;
+}
+
+bool compare2(pair<int, int> pre, pair<int, int> next)
+{
+	if (pre.first > next.first)
+		return true;
+	else if (pre.first == next.first && pre.second < next.second)
+		return true;
+	else
+		return false;
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays)
+{
+	// 장르, 재생수
+	map<string, int> cnt_plays;
+
+	// 장르 , 재생수, 고유번호
+	unordered_map<string, vector<pair<int, int>>> album;
+
+	for (int i = 0; i < (int)genres.size(); i++)
+	{
+		if (cnt_plays.find(genres[i]) != cnt_plays.end())
+			cnt_plays[genres[i]] += plays[i];
+		else
+			cnt_plays[genres[i]] = plays[i];
+		album[genres[i]].push_back({plays[i], i});
+	}
+
+	// 장르수에 따른 정렬용 vector
+	vector<pair<string, int>> v_cnt_palys(cnt_plays.begin(), cnt_plays.end());
+	sort(v_cnt_palys.begin(), v_cnt_palys.end(), compare);
+
+	vector<int> answer;
+
+	for (auto a : v_cnt_palys)
+	{
+		int cnt = 0;
+		sort(album[a.first].begin(), album[a.first].end(), compare2);
+
+		for (int i = 0; i < album[a.first].size() && i < 2; i++)
+			answer.push_back(album[a.first][i].second);
+	}
+
+	return answer;
+}
