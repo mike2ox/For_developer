1ee518970d7d56eb1be906a488b78261361d6fc3
mike2ox <firefinger07@gmail.com>
2020-09-18 18:37:20 +0900

    Add other solution

--- ACM/Programmers/level_2/12899.cpp
+++ ACM/Programmers/level_2/12899.cpp
@@ -5,21 +5,54 @@
 #include <stack>
 using namespace std;
 
-string solution(int n) {
-    string answer = "";
-    vector<char> base = {'1', '2', '4'};
+string solution(int n)
+{
+	string answer = "";
+	vector<char> base = {'1', '2', '4'};
 	stack<char> stk;
 
-	while (n) {
-        n--;
-        stk.push(base[n % 3]);
+	while (n)
+	{
+		n--;
+		stk.push(base[n % 3]);
 		n /= 3;
 	}
 
-	while (!stk.empty()) {
+	while (!stk.empty())
+	{
 		answer += stk.top();
 		stk.pop();
 	}
 
-    return answer;
+	return answer;
+}
+
+// 25분
+string solution2(int n)
+{
+	string answer = "";
+
+	while (n)
+	{
+		int rest = n % 3;
+		n /= 3;
+
+		if (rest == 0)
+		{
+			answer += '4';
+			n--;
+		}
+		else if (rest == 1)
+		{
+			answer += '1';
+		}
+		else
+		{
+			answer += '2';
+		}
+	}
+
+	reverse(answer.begin(), answer.end());
+
+	return answer;
 }
